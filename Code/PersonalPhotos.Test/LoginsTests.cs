65f38bcd3a21e01e42a2aa4034cc6551f67e9eef
Posccis <lkzq33@gmail.com>
2022-11-15 20:53:52 -0300

    add(master): finished this class

--- Code/PersonalPhotos.Test/LoginsTests.cs
+++ Code/PersonalPhotos.Test/LoginsTests.cs
@@ -1,12 +1,15 @@
-using PersonalPhotos.Controllers;
-using Xunit;
-using Moq;
-using Core.Interfaces;
+﻿using Core.Interfaces;
+using Core.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PersonalPhotos.Controllers;
 using PersonalPhotos.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
-using Core.Models;
+using Xunit;
 
 namespace PersonalPhotos.Test
 {
@@ -14,51 +17,43 @@ namespace PersonalPhotos.Test
     {
         private readonly LoginsController _controller;
         private readonly Mock<ILogins> _logins;
-        private readonly Mock<IHttpContextAccessor> _accessor;
+        private readonly Mock<IHttpContextAccessor> _acessor;
 
         public LoginsTests()
         {
             _logins = new Mock<ILogins>();
-            
-
-            var session = Mock.Of<ISession>();
-            var httpContext = Mock.Of<HttpContext>(x => x.Session == session);
-
-            _accessor = new Mock<IHttpContextAccessor>();
-            _accessor.Setup(x => x.HttpContext).Returns(httpContext);
-
-            _controller = new LoginsController(_logins.Object, _accessor.Object);
+            ISession session = Mock.Of<ISession>();
+            HttpContext httpContext = Mock.Of<HttpContext>(x => x.Session == session);
+            _acessor = new Mock<IHttpContextAccessor>();
+            _acessor.Setup(x => x.HttpContext).Returns(httpContext);
+            _controller = new LoginsController(_logins.Object, _acessor.Object);
         }
 
         [Fact]
-        public void Index_GivenNorReturnUrl_ReturnLoginView()
+        public void Index_GivenReturnUrl_ReturnLogInView() 
         {
-            var result = (_controller.Index() as ViewResult);
-
+            ViewResult result = (_controller.Index() as ViewResult);
             Assert.NotNull(result);
-            Assert.Equal("Login", result.ViewName, ignoreCase: true);
+            Assert.Equal("Login", result.ViewName, ignoreCase: true); 
         }
 
         [Fact]
-        public async Task Login_GivenModelStateInvalid_ReturnLoginView()
+        public async Task Login_GivenModelStateInvalid_ReturnLoginView() 
         {
             _controller.ModelState.AddModelError("Test", "Test");
-
-            var result = await _controller.Login(Mock.Of<LoginViewModel>()) as ViewResult;
+            ViewResult result = await _controller.Login(Mock.Of<LoginViewModel>()) as ViewResult;
             Assert.Equal("Login", result.ViewName, ignoreCase: true);
+            
         }
 
         [Fact]
-        public async Task Login_GivenCorrectPassword_RedirectToDisplayAction()
+        public async Task Login_GivenCorrectPassword_RedirectToDisplayAction() 
         {
             const string password = "123";
-            var modelView = Mock.Of<LoginViewModel>(x=> x.Email == "a@b.com" && x.Password== password);
-            var model = Mock.Of<User>(x=> x.Password == password);
-
-            _logins.Setup(x => x.GetUser(It.IsAny<string>())).ReturnsAsync(model);
-
+            LoginViewModel modelView = Mock.Of<LoginViewModel>(x => x.Email == "a@b.com" && x.Password == password);
+            User model = Mock.Of<User>(x => x.Password == password);
+            _logins.Setup(x => x.GetUser(It.IsAny<String>())).ReturnsAsync(model);
             var result = await _controller.Login(modelView);
-
             Assert.IsType<RedirectToActionResult>(result);
         }
     }
---
+++ Code/PersonalPhotos.Test/PhotoTests.cs
@@ -0,0 +1,40 @@
+﻿using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PersonalPhotos.Controllers;
+using PersonalPhotos.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PersonalPhotos.Test
+{
+    public class PhotoTests
+    {
+
+        [Fact]
+        public async Task Upload_GivenFileName_ReturnsDisplayAction() 
+        {
+            //arrange
+            ISession session = Mock.Of<ISession>();
+            session.Set("User", Encoding.UTF8.GetBytes("a@b.com"));
+            HttpContext context = Mock.Of<HttpContext>(x => x.Session == session);
+            IHttpContextAccessor accessor = Mock.Of<IHttpContextAccessor>(x => x.HttpContext == context);
+            IFileStorage fileStorage = Mock.Of<IFileStorage>();
+            IKeyGenerator keyGenerator = Mock.Of<IKeyGenerator>();
+            IPhotoMetaData photoMetaData = Mock.Of<IPhotoMetaData>();
+            IFormFile formFile = Mock.Of<IFormFile>();
+            PhotoUploadViewModel model = Mock.Of<PhotoUploadViewModel>(x => x.File == formFile);
+            PhotosController controller = new PhotosController(keyGenerator, accessor, photoMetaData, fileStorage);
+
+            //act
+            var result = await controller.Upload(model) as RedirectToActionResult;
+
+            //assert
+            Assert.Equal("Display", result.ActionName, ignoreCase: true);
+        }
+    }
+}
